5a212e0261be11c5700c9144ce83c04b2af2f79a
= <=>
2018-10-04 21:55:13 +0800

    add

--- 数据结构/大话数据结构/线性表/链式存取.c
+++ 数据结构/大话数据结构/线性表/链式存取.c
@@ -11,6 +11,26 @@ typedef struct Node
 
 typedef struct Node *LinkList;                  /*定义LinkList*/
 
+
+/*初始条件：顺序线性表L已经存在，1 <= i <= ListLength(L)*/
+/*操作结果：用e返回L中第i个数据元素的值*/
+Status GetElem(LinkList L, int i, ElemType *e)
+{
+    int j;
+    LinkList p;             /*声明一个结点P*/
+    p = L->next;            /*让p指向链表中L的第1个结点*/
+    j = 1;                  /*j为计数器*/
+    while(p && j<i)         /*p不为空或者计数器j还没有等于i时，循环继续*/
+    {
+        p = p->next;
+        ++j;
+    }
+
+    
+}
+
+
+
 int main(int argc, char const *argv[])
 {
     
