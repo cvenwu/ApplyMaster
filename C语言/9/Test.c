f6281d8b7ff5863089769d41a55c15ad9e14e4e6
= <=>
2018-10-05 15:00:40 +0800

    add pointer Test source file

---
+++ C语言/9/Test.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a = 3;
+    int *p = &a;                /* 指针变量赋值只能赋给地址，赋值方法一 */
+    int * p1;                   /* *与指针变量名字之间可以有空格 */
+    p1 = &a;                    /* 赋值方法二 */
+    *p1 = 333;
+    printf("%d\n", *p);
+    
+    return 0;
+}
