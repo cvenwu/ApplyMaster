cf304e660ff0508d73ba594af95c79ba5adee53a
= <=>
2018-10-06 10:01:55 +0800

    add 9-5.c

---
+++ C语言/9/9-5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    /* code */
+    int i, j, t;
+    int a, b, c, d, e;
+    int *p[5] = { &a, &b, &c, &d, &e };         //将a,b,c,d,e的内存地址分别赋给数组中的各个元素
+
+    scanf("%d,%d,%d,%d,%d", p[0], p[1], p[2], p[3], p[4]);
+    for(i = 0; i < 4; i++)
+        for(j = i+1; j < 5; j++)
+            if(*p[i] > *p[j])
+            {
+                t = *p[i];
+                *p[i] = *p[j];
+                *p[j] = t;
+            }
+
+    for(i = 0; i < 5; i++)                      //显示排序后的结果
+        printf("%d ", *p[i]);
+
+
+    return 0;
+}
