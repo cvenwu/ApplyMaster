41f7ffd4990c75b51a1a8e59c34b8e0089c19699
= <=>
2018-10-11 19:59:10 +0800

    add some files

--- 数据结构/大话数据结构/第八章查找/顺序表查找.c
+++ 数据结构/大话数据结构/第八章查找/顺序表查找.c
@@ -64,10 +64,58 @@ int Binary_Search(int *a, int n, int key)
     return 0;
 }
 
+/*插值查找*/
+int InsertValue_Search(int *a, int n, int key)
+{
+    int low, high, mid;
+    low = 1, high = n;                      /*定义最低下标和最高下标*/
+    while(low <= high)                      
+    {
+        mid = (low + ((key - a[low])/(a[high] - a[low]))) / 2;             /*折半*/
+        if(key < a[mid])                    /*若查找值比中值小*/
+            high = mid - 1;                 /*最高下标调整到中标小一位*/
+        else if(key > a[mid])               /*若查找值比中值大*/
+            low = mid + 1;                  /*最高下标调整到中标大一位*/
+        else                                /*若相等，则说明mid即为查找到的位置*/
+            return mid;
+    }
+
+    return 0;
+}
+
+/*斐波那契查找*/
+int Fibonacci_Search(int *a, int n, int key)
+{
+    int low, high, mid, i, k;
+    low = 1;                                    /*定义最低下标为记录首位*/
+    high = n;                                   /*定义最高下标为记录末位*/
+    k = 0;
+    while(n > F[k] - 1)                         /*计算n位于斐波那契数列的位置*/
+        k++;
+
+    for(i = n; i < F[k] - 1; i++)               /*将不满的数值补全*/
+        a[i] = a[n];
+
+    while(low <= high)
+    {
+        mid = low + F[k-1] - 1;             /*计算当前分隔的下标*/
+        if(key < a[mid])                    /*若查找记录小于当前分隔记录*/
+        {
+            high = mid - 1;                 /*最高下标调整到分隔下标mid-1处*/
+            k = k-1;                        /*斐波那契数列下标减一位*/
+        }
+        else if(key > a[mid])               /*若查找记录大于当前分隔记录*/
+        {
+            
+        }
+    }
+}
+
 
 int main(int argc, char const *argv[])
 {
     int a[] = {0, 1, 16, 24, 35, 57, 59, 62, 73, 88, 99};
-    printf("%d\n", Binary_Search(a, 10, 62));
+    //printf("%d\n", Binary_Search(a, 10, 62));
+    printf("%d\n", InsertValue_Search(a, 10, 62));
     return 0;
 }
