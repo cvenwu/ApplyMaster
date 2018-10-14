5ed20323cb66aa65781ac0e7b0b5cf181bb3aa7b
= <=>
2018-10-14 19:48:37 +0800

    add some files

---
+++ 数据结构/大话数据结构/第九章排序/直接插入排序/直接插入排序.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+
+/*对顺序表L作直接插入排序*/
+void InsertSort(SqList *L)
+{
+    int i, j;
+    for(i = 2; i <= L->length; i++)
+    {
+        if(L->r[i] < L->r[i-1])         /*需将L->r[i]插入有序子表*/
+        {
+            L->r[0] = L->r[i];
+            for(j = i - 1; L->r[j] > L->r[0]; j--)
+                L->r[j+1] = L->r[j];        /*记录后移*/
+            L->r[j+1] = L->r[0];
+        }
+    }
+}
+
+
+int main(int argc, char const *argv[])
+{
+    
+    return 0;
+}
