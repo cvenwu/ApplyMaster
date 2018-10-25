bfab6acdb73b0062a4c6f3552677cab3e96794b3
= <=>
2018-10-25 20:27:05 +0800

    add files

---
+++ C语言/教材第二轮复习/6/PPT上的代码/计算个数.c
@@ -0,0 +1,22 @@
+/*
+ 【例】计算用户输入的两正整数之间的所有整数中0，1，2，…，9数码的个数。
+ 例如，101～104之间总共包含四个整数101，102，103，104，
+
+ 其中0的个数为4，1的个数为5，2、3、4的个数都为1，其余数码没出现都为0。
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char const *argv[])
+{
+    int num1, num2, i;
+    printf("Please input two numbers : ");
+    scanf("%d %d", &num1, &num2);
+    for(i = num1; i <= num2; i++)
+    {
+        
+    }
+    return 0;
+}
