7fadf567423d56af7bc2e604303f822eb78aba7f
= <=>
2018-10-27 15:47:45 +0800

    add source files

---
+++ C语言/教材第二轮复习/11/Test2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+union myun
+{
+    struct {
+        int x, y, z;
+    } u;
+    int k;
+} a;
+
+
+int main(int argc, char const *argv[])
+{
+    a.u.x = 4;
+    a.u.y = 5;
+    a.u.z = 6;
+    a.k = 0;
+    printf("%d %d %d %d \n", a.u.x, a.u.y, a.u.z, a.k);
+    return 0;
+}
+
+i
